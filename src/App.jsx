8e4bc39377cb683f584cbbb69a47d5778252e7f4
Jan Gross <jan.gross.cze@gmail.com>
2024-02-10 17:09:45 +0100

    starting project: RANDOM COLOR GENERATOR

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Accordian from './components/accordion'
+import RandomColor from './components/random-color'
 
 function App() {
 
   return (
     <>
-     <Accordian />
+     {/* <Accordian /> */}
+     <RandomColor />
     </>
   )
 }
