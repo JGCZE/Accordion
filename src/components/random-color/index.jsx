cd44c604a05ff10bbf94f667a5a89a78508d0e02
Jan Gross <jan.gross@regiojet.cz>
2024-02-14 10:05:31 +0100

    final Random Generator

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -2,22 +2,40 @@ import { useState } from "react"
 import "./styles.css"
 
 const RandomColor = () => {
-  const [bgColor, setBgColor] = useState('rgb(230, 30, 33)')
+  const [colorType, setColorType] = useState('')
+  const [bgColor, setBgColor] = useState('rgb(0, 0,0)')
   
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 255)
+  };
 
   const getRandomColor = () => {
-    const randomNumber = Math.floor(Math.random() * 255)
-    console.log(randomNumber);
-    const newColor = (`rgb(${randomNumber} ${randomNumber} ${randomNumber})`)
-    setBgColor(newColor)
-    console.log(bgColor);
+    if(colorType == 'rgb') {
+    const newColor = (`rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`)
+    setBgColor(newColor) 
+    } 
+    
+    if (colorType == 'hex') {
+      const newColor = `#${Math.floor(Math.random()*16777215).toString(16)}`
+      setBgColor(newColor)
+    }
   }
 
+  console.log(bgColor)
+  console.log(colorType);
+
   return (
-    <div className='container' style={{ backgroundColor: bgColor }}>
+    <div className='container' style={{ 
+      width: '300px',
+      height: '150px',
+      backgroundColor: bgColor
+     }}>
+      <button onClick={() => setColorType("hex")}>Create HEX</button>
+      <button onClick={() => setColorType("rgb")}>Create RGB</button>
       <button onClick={getRandomColor}>Generate random Color</button>
+      <p className="colorSection"> {bgColor} </p>
     </div>
   )
 }
 
-export default RandomColor
+export default RandomColor;
