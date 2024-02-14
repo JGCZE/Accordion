8b9b30e4f23c4009b9ca351df6307b697b63db6f
Jan Gross <jan.gross.cze@gmail.com>
2024-02-14 08:58:05 +0100

    save

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useState } from "react"
+import "./styles.css"
 
 const RandomColor = () => {
+  const [bgColor, setBgColor] = useState('rgb(230, 30, 33)')
+  
+
+  const getRandomColor = () => {
+    const randomNumber = Math.floor(Math.random() * 255)
+    console.log(randomNumber);
+    const newColor = (`rgb(${randomNumber} ${randomNumber} ${randomNumber})`)
+    setBgColor(newColor)
+    console.log(bgColor);
+  }
+
   return (
-    <div>RandomColor</div>
+    <div className='container' style={{ backgroundColor: bgColor }}>
+      <button onClick={getRandomColor}>Generate random Color</button>
+    </div>
   )
 }
 
